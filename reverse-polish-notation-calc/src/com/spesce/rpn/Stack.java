cfd0068e2caa24d7168a666317ea11baf914f135
Steve <stevepesce879@gmail.com>
2020-11-22 16:18:49 -0500

    implemented my stack instead, added cli

---
+++ reverse-polish-notation-calc/src/com/spesce/rpn/Stack.java
@@ -0,0 +1,67 @@
+package com.spesce.rpn;
+
+import java.util.Arrays;
+
+/**
+ * Created By Steve P. on 11/15/2020
+ * original package: com.company
+ * ------------------------------------------------------------------------------------------
+ */
+public class Stack {
+    private Node top;
+    private int size;
+
+    public Stack() {
+        size = 0;
+    }
+
+    public Stack(double[] dataArray){
+        Arrays.stream(dataArray).forEach(data -> push(data));
+        size = dataArray.length;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+    //add int to top of stack
+    public void push(double data){
+        if(top == null)
+            top = new Node(data);
+        else
+        {
+            Node current = new Node(data,top);
+            top = current;
+        }
+        size++;
+    }
+    //get data from top, but dont remove it
+    public double peekTop() {
+        return top.getData();
+    }
+    //remove and get data from top of stack
+    public double pop() {
+        final double data = top.getData();
+        top = top.getNext();
+        size--;
+        return data;
+    }
+    //print stack to console, point at top element
+    public void print() {
+        if(top == null)
+            System.out.println("The stack is empty");
+
+        Node current = top;
+        System.out.println(current.getData() + " <--");
+        current = current.getNext();
+
+        while(current != null)
+        {
+            System.out.println(current.getData());
+            current = current.getNext();
+        }
+    }
+}
