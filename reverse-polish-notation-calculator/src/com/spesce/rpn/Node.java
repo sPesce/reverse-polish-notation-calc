7b5a1ad98049aa46df5d38dfb6588524bd1e7cdd
Steve <stevepesce879@gmail.com>
2020-11-22 15:29:07 -0500

    implemented my stack instead, added cli

---
+++ reverse-polish-notation-calculator/src/com/spesce/rpn/Node.java
@@ -0,0 +1,35 @@
+package com.company;
+
+/**
+ * Created By Steve P. on 11/15/2020
+ * original package: com.company
+ * ------------------------------------------------------------------------------------------
+ */
+public class Node {
+    private int data;
+    private Node next;
+
+    public Node() {
+    }
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
